855ed6a7a792ac2aabf6d67c0df02f7a6da3352e
vgn40 <117641530+vgn40@users.noreply.github.com>
2024-10-09 11:37:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,67 @@
     Håndterer brugerkonti, herunder registrering, autentificering og profiladministration.
     Behandler login, logout, passwordhåndtering og brugerroller.
 """
-
-from Flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response
 import requests
+import sqlite3
 
 app = Flask(__name__)
 
-# DATABASE - skal skiftes til sqlite
-users_db = []
-data = requests.get('https://dummyjson.com/users/1')
-users_db.append(data.json())
+data = requests.get('https://dummyjson.com/products/')
+products = data.json()['products']
+
+def connect_db():
+    conn = sqlite3.connect('products.db')
+    return conn
+
+def create_table():
+    conn = connect_db()
+    conn.execute('''CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    category TEXT,
+    price REAL,
+    stock INTEGER,
+    brand TEXT,
+    sku TEXT,
+    weight REAL,
+    image_url TEXT,
+    thumbnail_url TEXT
+);''')
+    conn.commit()
+    conn.close()
+
+def insert_products(products):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    for product in products:
+        cursor.execute('''INSERT OR REPLACE INTO products 
+            (id, title, description, category, price, stock, brand, sku, weight, image_url, thumbnail_url) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+            (
+                product.get('id'), 
+                product.get('title'), 
+                product.get('description'), 
+                product.get('category'), 
+                product.get('price'), 
+                product.get('stock'), 
+                product.get('brand', ''), 
+                product.get('sku'),   
+                product.get('weight', 0),  
+                product['images'][0],      
+                product.get('thumbnail', '')
+            )
+        )
+    conn.commit()
+    conn.close()
+
+
+create_table()
+insert_products(products)
+
+
 
 # find bruger via username
 def find_user_by_username(username):
@@ -77,16 +128,78 @@ def logout():
     return jsonify(), 201
 
 @app.route('/product', methods=['GET'])
-def get_product():
-    data = request.headers.get('Authorization')
-    if not data:
-        return jsonify({'message': 'User not logged in'}), 401
+def get_all_product():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM products')
+    products = cursor.fetchall()
+    conn.close()
+    
+    product_list = []
+    for product in products:
+        product_list.append({
+            'id': product[0],
+            'title': product[1],
+            'description': product[2],
+            'category': product[3],
+            'price': product[4],
+            'stock': product[5],
+            'brand': product[6], 
+            'sku': product[7],
+            'weight': product[8],
+            'image_url': product[9],
+            'thumbnail_url': product[10]
+        })
+    return jsonify(product_list), 200
 
-    user = find_user_by_username(data)
-    if not user:
-        return jsonify({'message': 'User not found'}), 404
+@app.route('/product/<int:id>', methods=['GET'])
+def get_product_by_id(id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM products WHERE id=?',(id,))
+    products = cursor.fetchall()
+    conn.close()
+    
+    product_list = []
+    for product in products:
+        product_list.append({
+            'id': product[0],
+            'title': product[1],
+            'description': product[2],
+            'category': product[3],
+            'price': product[4],
+            'stock': product[5],
+            'brand': product[6], 
+            'sku': product[7],
+            'weight': product[8],
+            'image_url': product[9],
+            'thumbnail_url': product[10]
+        })
+    return jsonify(product_list), 200
 
-    return jsonify(users_db), 200
-
+@app.route('/product/category/<string:category>', methods=['GET'])
+def get_product_by_category():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM products WHERE category=?',(category,))
+    products = cursor.fetchall()
+    conn.close()
+    
+    product_list = []
+    for product in products:
+        product_list.append({
+            'id': product[0],
+            'title': product[1],
+            'description': product[2],
+            'category': product[3],
+            'price': product[4],
+            'stock': product[5],
+            'brand': product[6], 
+            'sku': product[7],
+            'weight': product[8],
+            'image_url': product[9],
+            'thumbnail_url': product[10]
+        })
+    return jsonify(product_list), 200
 
 app.run(debug=True, host='0.0.0.0')
